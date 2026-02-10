939fb99651a7988b67ac3e1b5227ca3762500b10
dipalibalas <dipalimbalas@gmail.com>
2026-02-10 20:22:16 +0530

    Exploring the routes and http methods

---
+++ homework.md
@@ -0,0 +1,22 @@
+- Create a repository
+- Initialize the repository
+- node_modules, package.json, package-lock.json
+- Install express
+- Create a server
+- Listen to port 7777
+- Write request handlers for /test, /hello
+- Install nodemon and update scripts inside package.json
+- What are dependencies
+- What is the use of "-g" while npm install
+- Difference between caret and tilde (^ vs ~)
+
+Ep. 02
+
+- Play with routes and route extensions ex. /hello, /, hello/2, /xyz
+- Order of the routes matter a lot
+- Install postman app and make a workspace/collection > test API call
+- Write logic to handle GET, POST, PATCH, DELETE API calls and test them on postman
+- Explore routing and use of ?, +, (), * in the routes
+- Use of regex in routes /a/, /.*fly$/
+- Reading the query params in the routes
+- Reading the dynamic routes
--- src/app.js
+++ src/app.js
@@ -2,14 +2,35 @@ const express = require("express");
 
 const app = express();
 
-// app.use((req,res)=>{
-//     res.send("Hello from the server")     // request handler
-// })
+// This will only handle GET requests to /user
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Akshay", lastName: "Saini" });
+});
+app.get("/user", (req, res) => {
+ res.send({ firstName: "Dipali", lastName: "Balas" });
+});
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from the test server")     // request handler
-})
+// Handle POST request → Save data to the database
+app.post("/user", (req, res) => {
+ // saving data to DB
+ res.send("Data successfully saved to the DB!");
+});
 
-app.listen(3000,()=>{
-    console.log("Server is running on 3000")
+// Handle DELETE request → Delete user data
+app.delete("/user", (req, res) => {
+ res.send("Deleted successfully!!");
+});
+
+
+app.use("/test", (req, res) => {
+  res.send("Hello from the test server"); // request handler
+});
+
+app.use((req, res) => {
+  res.send("Hello from the server"); // request handler
+});
+
+app.listen(7777, () => {
+  console.log("Server is running on 7777");
 });
