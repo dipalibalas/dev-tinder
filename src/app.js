954e5df2b608e45e1bb2ff0dd1f5b046d89ba8c6
dipalibalas <dipalimbalas@gmail.com>
2026-02-17 13:57:08 +0530

    Dynamic data sigup api done

--- src/app.js
+++ src/app.js
@@ -3,6 +3,21 @@ const dotenv = require("dotenv");
 dotenv.config();
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  // Creating a new instance of the user model
+  const user = new User({
+    firstName: "Dipali",
+    lastName: "Balas",
+    emailId: "deep@gmail.com",
+    age: 30,
+    password: "deep@123",
+  });
+
+  await user.save();
+  res.send("User added successfully!");
+});
 
 // First of all connet to DB then do server to listen
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,28 +1,27 @@
-import mongoose from 'mongoose';
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName:{
-        type:String
-    },
-    lastNAme:{
-        type:String
-    },
-    emailId:{
-        type:String
-    },
-    password:{
-        type:String
-    },
-    age:{
-        type:Number
-    },
-    gender:{
-        type: String
-    }
+  firstName: {
+    type: String,
+  },
+  lastNAme: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  gender: {
+    type: String,
+  },
 });
 
 // const User = mongoose.model("User",userSchema);
 // module.exports = User;
 
-
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
