1e0b8ff50dbb1b7a1cf858c670cd967078b3bd0b
dipalibalas <dipalimbalas@gmail.com>
2026-02-17 14:11:39 +0530

    Dynamic data sigup api done with json middleare

--- src/app.js
+++ src/app.js
@@ -5,23 +5,25 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+// json data middaleware
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
   // Creating a new instance of the user model
-  const user = new User({
-    firstName: "Dipali1",
-    lastName: "Balas",
-    emailId: "deep123@gmail.com",
-    age: 30,
-    password: "deep@123",
-  });
+  // const user = new User({
+  //   firstName: "Dipali1",
+  //   lastName: "Balas",
+  //   emailId: "deep123@gmail.com",
+  //   age: 30,
+  //   password: "deep@123",
+  // });
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User added successfully!");
   } catch (error) {
     res.status(400).send("Error in user data saving...");
   }
-
- 
 });
 
 // First of all connet to DB then do server to listen
