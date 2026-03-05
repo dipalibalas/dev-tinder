6f10c6982cb38fb54d1599c3bfd53acbe5e81b21
dipalibalas <dipalimbalas@gmail.com>
2026-03-06 05:06:14 +0530

    Built chat api with web socket

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http");
 
 app.use(
   cors({
@@ -21,17 +22,23 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
+
+const server = http.createServer(app);
+initializeSocket(server);
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
 
 // First of all connet to DB then do server to listen
 connectDB()
   .then(() => {
     console.log("Database connection established...");
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log("Server is running on", process.env.PORT);
     });
   })
